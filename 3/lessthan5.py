a23b55bbb96fc9d3600690c442a5f7c131639427
biraaaaj <birajbhattarai182@gmail.com>
2018-11-17 23:09:59 -0800

    lessthan 5 updated successfully

--- 3/lessthan5.py
+++ 3/lessthan5.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 a=[1,1,2,3,5,8,13,21,34,55,89]
+b=[]
+
 print ("The numbers less than 5 are:")
 for i in a:
-	if i < 5:
-		print (i)
+	if i < 5 and i not in b:
+		b.append(i)
+print (b)
+			
+
+
+		
+			
