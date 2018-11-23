f6246bf9ae27faf60d6c1385fa07dcde7f375d45
biraaaaj <birajbhattarai182@gmail.com>
2018-11-22 22:23:50 -0800

    production set completed

---
+++ door/proset.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+swaps = {'ba':'ab', 'cb':'bc', 'ca':'ac'}
+string = 'abcabcba'
+sorted_string = ''.join(sorted(string))
+
+while string != sorted_string:
+	print(string)
+	for keys in swaps:
+		if(keys in string):
+			string = string.replace(keys,swaps[keys],1)
+
+print(string)
